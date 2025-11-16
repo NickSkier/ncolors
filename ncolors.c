e7a07b1504ca6c0e87ffa295084f1ea5c7b03719
NickSkier <nickskier02@gmail.com>
2025-11-16 16:37:54 +0300

    refactor: foundation for better functionality

--- ncolors.c
+++ ncolors.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
 
-int main(int argc, char *argv[])
-{
+typedef enum {
+	ALL_COLORS,
+} OutputType;
+
+void init_ncurses() {
 	initscr();
 	if(has_colors() == FALSE)
 	{
@@ -11,17 +15,39 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	start_color();
+	use_default_colors();
+}
 
-	int color = 0;
-	int cell_size = 5;
-	for (int i = 1; i <= 16; ++i)
-		for (int j = 1; j <= 16; ++j) {
+void print_all_colors() {
+	short color = 0;
+	short cell_size = 5;
+	for (short i = 1; i <= 16; ++i)
+		for (short j = 1; j <= 16; ++j) {
 			if (color++ == 256) break;
 			init_pair(color, COLOR_BLACK, color);
 			attron(COLOR_PAIR(color));
 			mvprintw(j-1, i*cell_size-cell_size, "%*d", cell_size, color);
-			// attroff(COLOR_PAIR(color));
 		}
+	attroff(COLOR_PAIR(color));
+}
+
+int main(int argc, char *argv[])
+{
+	OutputType output;
+	if (argc == 1) output = ALL_COLORS;
+	else {
+		printf("Wrong number of arguments\n");
+		exit(1);
+	}
+
+	if (output == ALL_COLORS) {
+		init_ncurses();
+		print_all_colors();
+	}
+	else {
+		printf("Wrong arguments values\n");
+		exit(1);
+	}
 
 	getch();
 	endwin();
